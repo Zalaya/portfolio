eba429bef06e73c241385d2fa92c8d355a36b85c
Zalaya <enol.corzal@gmail.com>
2024-10-05 22:07:03 +0200

    feat(configuration): add more tailwind configuration to the preview.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import "./styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
